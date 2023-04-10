e1d9da242f5ea73cc5151c6287974e4f05c4b680
SUPS0110 <suprima0110@gmail.com>
2023-04-11 00:40:18 +0545

    updates

--- addinfo.php
+++ addinfo.php
@@ -5,17 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> NOTICE FORM</title>
+    <link rel="stylesheet" href="./form.css">
 </head>
 <body>
     <form action="add.php" method="get">
     <div class="box">
-        <label>TITLE:</label>
-        <input name="title" type="text" placeholder="Enter the title" required>
-        <label>NOTICE:</label>
-        <textarea name="desc" type="text" placeholder="Enter the title" required></textarea>
-        <label>DATE:</label>
-        <input name="date" type="date" required >
-        <button type="submit">ADD</button>
+       <div class="list"><label>TITLE:</label>
+        <input name="title" type="text" placeholder="Enter the title" required></div>
+        <div class="list"><label>NOTICE:</label>
+        <textarea name="desc" type="text" placeholder="Enter the title" required></textarea></div>
+        <div class="list"><label>DATE:</label>
+        <input name="date" type="date" required ></div>
+        <div class="click"><button type="submit">ADD</button></div>
     </div>
     </form>
 </body>
--- editinfo.php
+++ editinfo.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> NOTICE FORM</title>
+    <title> EDIT FORM</title>
+    <link rel="stylesheet" href="./form.css">
 </head>
 <body>
     <form action="edit.php" method="get">
     <div class="box">
         <input name="id" type="number" value="<?php echo $_GET['id'];?>" hidden>
-        <label>TITLE:</label>
-        <input name="title" type="text" value="<?php echo $_GET['title'];?>" placeholder="Enter the title" required>
-        <label>NOTICE:</label>
-        <textarea name="desc" type="text" placeholder="Enter the title" required><?php echo $_GET['desc'];?></textarea>
-        <label>DATE:</label>
-        <input name="date" type="date" value="<?php echo $_GET['date'];?>" required>
-        <button type="submit">EDIT</button>
+        <div class="list"><label>TITLE:</label>
+        <input name="title" type="text" value="<?php echo $_GET['title'];?>" placeholder="Enter the title" required></div>
+        <div class="list"><label>NOTICE:</label>
+        <textarea name="desc" type="text" placeholder="Enter the title" required><?php echo $_GET['desc'];?></textarea></div>
+        <div class="list"><label>DATE:</label>
+        <input name="date" type="date" value="<?php echo $_GET['date'];?>" required></div>
+    <div class="click"><button type="submit">EDIT</button></div>
     </div>
     </form>
 </body>
